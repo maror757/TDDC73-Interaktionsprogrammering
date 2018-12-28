517884b66044ecb80fcde30f529d39e31164811d
maror757 <maror757@student.liu.se>
2018-12-28 11:31:35 +0100

    Password meter module finished

--- Projekt1/app/src/main/java/com/example/projekt1/PasswordStrengthMeter.java
+++ Projekt1/app/src/main/java/com/example/projekt1/PasswordStrengthMeter.java
@@ -18,6 +18,11 @@ public class PasswordStrengthMeter extends LinearLayout {
     private ProgressBar progressBar;
     private TextView textView;
 
+
+    private int WEAK = 0;
+    private int MEDIUM = 33;
+    private int STRONG = 66;
+    private int VERY_STRONG= 100;
     private int progressStatus = 0;
     // Constructor
     public PasswordStrengthMeter(Context cxt) {
@@ -29,9 +34,87 @@ public class PasswordStrengthMeter extends LinearLayout {
     }
 
     //Create function that determines password strength
-    private void getPasswordStrength(String pw) {
+    private int getPasswordStrength(String password) {
 
-        String password = pw;
+        //--------REQUIREMENTS--------
+        int REQUIRED_LENGTH = 6;
+        int GOOD_LENGTH = 10;
+        boolean REQUIRE_SPECIAL_CHARACTERS = true;
+        boolean REQUIRE_DIGITS = true;
+        boolean REQUIRE_LOWER_CASE = true;
+        boolean REQUIRE_UPPER_CASE = true;
+
+        int currentScore = 0;
+        boolean sawUpper = false;
+        boolean sawLower = false;
+        boolean sawDigit = false;
+        boolean sawSpecial = false;
+
+            for (int i = 0; i < password.length(); i++)
+            {
+                char c = password.charAt(i);
+
+                if (!sawSpecial && !Character.isLetterOrDigit(c))
+                {
+                    currentScore += 1;
+                    sawSpecial = true;
+                }
+                else
+                {
+                    if (!sawDigit && Character.isDigit(c))
+                    {
+                        currentScore += 1;
+                        sawDigit = true;
+                    }
+                    else
+                    {
+                        if (!sawUpper || !sawLower)
+                        {
+                            if (Character.isUpperCase(c))
+                                sawUpper = true;
+                            else
+                                sawLower = true;
+                            if (sawUpper && sawLower)
+                                currentScore += 1;
+                        }
+                    }
+                }
+            }
+
+            if (password.length() > REQUIRED_LENGTH)
+            {
+                if ((REQUIRE_SPECIAL_CHARACTERS && !sawSpecial) || (REQUIRE_UPPER_CASE && !sawUpper) || (REQUIRE_LOWER_CASE && !sawLower) || (REQUIRE_DIGITS && !sawDigit))
+                {
+                    currentScore = 1;
+                }
+                else
+                {
+                    currentScore = 2;
+                    if (password.length() > GOOD_LENGTH)
+                    {
+                        currentScore = 3;
+                    }
+                }
+            }
+            else
+            {
+                currentScore = 0;
+            }
+
+            switch (currentScore)
+            {
+                case 0:
+                    return WEAK;
+                case 1:
+                    return MEDIUM;
+                case 2:
+                    return STRONG;
+                case 3:
+                    return VERY_STRONG;
+                default:
+            }
+
+            return VERY_STRONG;
     }
 
     private void setLayout()
@@ -95,26 +178,31 @@ public class PasswordStrengthMeter extends LinearLayout {
             @Override
             public void afterTextChanged(Editable pw) {
                 String password = pw.toString();
-                progressStatus = password.length()*10;
-                if (progressStatus > 50 && progressStatus < 75)
+                //progressStatus = password.length()*10;
+                progressStatus = getPasswordStrength(password);
+                if (progressStatus == WEAK)
+                {
+                    textView.setText("Weak");
+                }
+                else if(progressStatus == MEDIUM)
+                {
+                    progressBar.setProgressDrawable(res.getDrawable(R.drawable.redprogress));
+                    textView.setText("Medium");
+                }
+                else if(progressStatus == STRONG)
                 {
                     progressBar.setProgressDrawable(res.getDrawable(R.drawable.yellowprogress));
-                    textView.setText("Good");
-                }
-                else if(progressStatus > 75)
-                {
-                    progressBar.setProgressDrawable(res.getDrawable(R.drawable.greenprogress));
                     textView.setText("Strong");
                 }
                 else
                 {
-                    progressBar.setProgressDrawable(res.getDrawable(R.drawable.redprogress));
-                    textView.setText("Weak");
+                    progressBar.setProgressDrawable(res.getDrawable(R.drawable.greenprogress));
+                    textView.setText("Very Strong");
                 }
 
                 progressBar.setProgress(progressStatus);
                 //TODO: Set this function to generate a value between 0-100 based on pw strength.
-                getPasswordStrength(password);
+                //getPasswordStrength(password);
             }
         });
     }
